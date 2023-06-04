f47882dfc53df531231f78407a6029c2a9998e9e
Márcio Alexandre <110576090+marcioacfilho@users.noreply.github.com>
2023-06-03 22:37:43 -0300

    atualizando menu

--- menu-sistema.php
+++ menu-sistema.php
@@ -1,19 +1,19 @@
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MRFLIX</a>
+    <a class="navbar-brand text-white" href="#">MRFLIX</a>
     <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <a class="nav-link" href="listar-jogos.php">Filmes</a>
+            <a class="nav-link text-white" href="listar-jogos.php">Filmes</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="listar-usuarios.php">Usuários</a>
+            <a class="nav-link text-white" href="listar-usuarios.php">Usuários</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="index.php"><b>SAIR</b></a>
+            <a class="nav-link text-danger" href="index.php"><b>SAIR</b></a>
         </li>
       </ul>
     </div>
--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg bg-dark">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">MRFLIX</a>
+    <a class="navbar-brand text-white" href="#">MRFLIX</a>
     <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
