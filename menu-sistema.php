359e5aae5ec45e449e585f6347d58e1943812489
Márcio Alexandre <110576090+marcioacfilho@users.noreply.github.com>
2023-06-04 21:18:54 -0300

    Update menu-sistema.php

--- menu-sistema.php
+++ menu-sistema.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-            <a class="nav-link text-white" href="listar-jogos.php">Filmes</a>
+            <a class="nav-link text-white" href="listar-filmes.php">Filmes</a>
         </li>
         <li class="nav-item">
             <a class="nav-link text-white" href="listar-usuarios.php">Usuários</a>
